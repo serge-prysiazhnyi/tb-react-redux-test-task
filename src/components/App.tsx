fdb5d66d01af2ef5d16c4673666fad89734c9e2b
SergeP <serge.prysiazhnyi@gmail.com>
2023-04-03 16:41:29 +0300

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import "../../resources/css/layout.scss"
+
+import React from "react";
+
+interface IProps {
+
+}
+
+export const App: React.FunctionComponent<IProps> = ({}: IProps) => {
+
+    return <div className={""}>
+        Application
+    </div>;
+};
